c777ab0db0366ca60c82c27ef6750d920724bdcd
serioushyeon <zz64446949@gmail.com>
2024-01-17 15:10:53 +0900

    Initial Commit

---
+++ BeaMinClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BeaMinClone
+//
+//  Created by jin on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
